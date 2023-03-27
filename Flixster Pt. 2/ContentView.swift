15950ed4b4992ac7a1330f1abbd19ddb7e564987
lewist13 <lewistpro@gmail.com>
2023-03-27 02:23:16 -0400

    Initial Commit

---
+++ Flixster Pt. 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Flixster Pt. 2
+//
+//  Created by TaeVon Lewis on 3/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flixster Pt. 2/Flixster_Pt__2App.swift
@@ -0,0 +1,17 @@
+//
+//  Flixster_Pt__2App.swift
+//  Flixster Pt. 2
+//
+//  Created by TaeVon Lewis on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Flixster_Pt__2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
